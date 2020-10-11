ab2888f0da128399df5c3d290367cd1c91eeca9d
kosukemakita <makitakosuke0114@gmail.com>
2020-10-11 13:15:53 +0900

    Initial Commit

---
+++ SeondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeondKadaiApp
+//
+//  Created by 蒔田幸祐 on 2020/10/11.
+//  Copyright © 2020 kousuke.makita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
