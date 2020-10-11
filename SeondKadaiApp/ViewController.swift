a9c994db05591655d8fa3283d5a7182a0ab9672e
kosukemakita <makitakosuke0114@gmail.com>
2020-10-11 14:18:06 +0900

    線維画面とUIアラインを追加

--- SeondKadaiApp/ViewController.swift
+++ SeondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func handle(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
